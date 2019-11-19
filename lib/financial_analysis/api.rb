a03eaab1572cda0a5788024cbdf4c67001601b11
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-19 22:23:15 +0000

    build class & method get_companies

---
+++ lib/financial_analysis/api.rb
@@ -0,0 +1,9 @@
+class FinancialAnalysis::API
+  # https://financialmodelingprep.com/api/v3/company/profile/MU
+  def self.get_companies
+    response = HTTParty.get("https://financialmodelingprep.com/api/v3/company/stock/list")
+    response["symbolsList"][0..50]
+    binding.pry
+  end
+  
+end
