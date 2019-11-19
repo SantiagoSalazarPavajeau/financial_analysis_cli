34b5948267373b04c92102fec179071c9a8dea9a
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-19 22:24:12 +0000

    requiring gems & files

---
+++ lib/financial_analysis.rb
@@ -0,0 +1,11 @@
+require "pry"
+require "httparty"
+require_relative "financial_analysis/version"
+require_relative "financial_analysis/api"
+require_relative "financial_analysis/cli"
+
+
+module FinancialAnalysis
+  class Error < StandardError; end
+  # Your code goes here...
+end
