0a1fd6b77784600caa9ed82f280cf5ec4ef14929
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-20 00:06:22 +0000

    initial CLI interface concept

--- lib/financial_analysis/api.rb
+++ lib/financial_analysis/api.rb
@@ -3,7 +3,7 @@ class FinancialAnalysis::API
   def self.get_companies
     response = HTTParty.get("https://financialmodelingprep.com/api/v3/company/stock/list")
     response["symbolsList"][0..50]
-    binding.pry
+    #binding.pry
   end
   
 end
--- lib/financial_analysis/cli.rb
+++ lib/financial_analysis/cli.rb
@@ -3,5 +3,17 @@ class FinancialAnalysis::CLI
   def start
     puts "Time for some Financial Analysis"
     FinancialAnalysis::API.get_companies
+    list_companies
+    menu
+  end
+  
+  def list_companies
+    puts "1. Apple"
+    puts "2. Facebook"
+    puts "3. GM"
+  end
+  
+  def menu
+    puts "Choose a company you would like more information on."
   end
 end
