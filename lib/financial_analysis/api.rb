7022d908d50f5d55a5ebe0055ee865d71332c728
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-20 23:58:33 +0000

    update get_companies method

--- lib/financial_analysis/api.rb
+++ lib/financial_analysis/api.rb
@@ -1,7 +1,8 @@
 class FinancialAnalysis::API
-  # https://financialmodelingprep.com/api/v3/company/profile/MU
+  # https://financialmodelingprep.com/api/v3/company/profile/AAPL,FB,GOOG
+  # https://financialmodelingprep.com/api/v3/company/stock/list
   def self.get_companies
-    response = HTTParty.get("https://financialmodelingprep.com/api/v3/company/stock/list")
+    response = HTTParty.get("https://financialmodelingprep.com/api/v3/company/profile/AAPL,FB,GOOG")
     response["symbolsList"][0..50]
     #binding.pry
   end
