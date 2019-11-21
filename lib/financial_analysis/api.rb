b9d9d9d14b8c10e810402d7b92b9d310b9f1a10d
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-21 00:16:32 +0000

    api class for objs

--- lib/financial_analysis/api.rb
+++ lib/financial_analysis/api.rb
@@ -3,8 +3,22 @@ class FinancialAnalysis::API
   # https://financialmodelingprep.com/api/v3/company/stock/list
   def self.get_companies
     response = HTTParty.get("https://financialmodelingprep.com/api/v3/company/profile/AAPL,FB,GOOG")
-    response["symbolsList"][0..50]
     #binding.pry
+  end
+  
+  def get_balance_sheet(ticker)
+    response = HTTParty.get("https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/#{ticker}")
+    balance_sheet = FinancialAnalysis::BalanceSheet.new(response)
+  end
+  
+  def get_income_statement(ticker)
+    response = HTTParty.get("https://financialmodelingprep.com/api/v3/financials/income-statement/#{ticker}")
+    income_statement = FinancialAnalysis::IncomeStatement.new(response)
+  end
+  
+  def get_profile(ticker)
+    response = HTTParty.get("https://financialmodelingprep.com/api/v3/company/profile/#{ticker}")
+    profile = FinancialAnalysis::Profile.new(response)
   end
   
 end
