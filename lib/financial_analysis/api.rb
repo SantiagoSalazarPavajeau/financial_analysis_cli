d7f770cce867468436b012777a7f0a2ac99b6dcb
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-26 23:58:30 +0000

    clean

--- lib/financial_analysis/api.rb
+++ lib/financial_analysis/api.rb
@@ -6,19 +6,6 @@ class FinancialAnalysis::API
     response["mostActiveStock"].each do |hash|
       FinancialAnalysis::Company.new(hash["ticker"])
     end
-    #binding.pry
   end
-  
-  
-  
-  # def get_income_statement(ticker)
-  #   response = HTTParty.get("https://financialmodelingprep.com/api/v3/financials/income-statement/#{ticker}")
-  #   income_statement = FinancialAnalysis::IncomeStatement.new(response)
-  # end
-  
-  # def get_profile(ticker)
-  #   response = HTTParty.get("https://financialmodelingprep.com/api/v3/company/profile/#{ticker}")
-  #   profile = FinancialAnalysis::Profile.new(response)
-  # end
-  
+
 end
--- lib/financial_analysis/company.rb
+++ lib/financial_analysis/company.rb
@@ -1,8 +1,4 @@
 class FinancialAnalysis::Company
-  # https://financialmodelingprep.com/api/v3/company/profile/MU
-  # https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/FB
-  # https://financialmodelingprep.com/api/v3/financials/income-statement/AAPL
-  # https://financialmodelingprep.com/api/v3/company/stock/list
   
   attr_accessor :ticker, :balance_sheets, :income_statements, :price, :beta, :volAvg, :mktCap, :lastDiv, :range, :changes, :changesPercentage, :companyName, :exchange, :industry, :website, :description, :ceo, :sector, :image
   
@@ -22,18 +18,5 @@ class FinancialAnalysis::Company
   def self.all
     @@all
   end
-  
-  # def get_balance_sheets(ticker)
-  #   response_balance = HTTParty.get("https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/#{ticker}")
-  #   self.balance_sheets = response_balance["financials"][0]
-  #   self.balance_sheets
-  #   binding.pry
-  # end
-  
-  # def get_income_statement(ticker)
-  #   response_income = HTTParty.get("https://financialmodelingprep.com/api/v3/financials/income-statement/#{ticker}")
-  #   self.income_statements = response_income["financials"][0]
-  #   self.income_statements
-  # end
   
 end
